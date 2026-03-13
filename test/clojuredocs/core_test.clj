6a4e8e88ae51213a62a7c5393f1de34d64eafcf3
Jordan Miller <jordan.miller@nubank.com.br>
2026-03-13 16:47:19 -0400

    Revert "improve placeholder test to be testing something useful"
    This reverts commit 8e5f827abc0b8654fe8da5a1ca1018ebbd83db1c.

--- test/clojuredocs/core_test.clj
+++ test/clojuredocs/core_test.clj
@@ -1,7 +1,6 @@
 (ns clojuredocs.core-test
-  (:require [clojure.test :refer :all]
-            [clojuredocs.core :as core]))
+  (:require [clojure.test :refer :all]))
 
-(deftest core-namespace-loads
-  (testing "core namespace loads successfully"
-    (is (find-ns 'clojuredocs.core))))
+(deftest placeholder-test
+  (testing "Placeholder — real tests coming soon"
+    (is (= 1 1))))
